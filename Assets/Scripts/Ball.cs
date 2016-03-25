d19553d7468b1a128a476edc2a6098923e919c59
Gage <gagelangdon@gmail.com>
2016-03-25 01:00:58 -0700

    autosave

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,40 +1,48 @@
-﻿using UnityEngine;
+﻿/// <summary>
+/// Ball.cs
+/// 
+/// Gage Langdon
+/// 3.24.2016
+/// 
+/// </summary>
+
+using UnityEngine;
 using System.Collections;
 
 public class Ball : MonoBehaviour {
 
-    //set the movement speed of the ball
+    // ball movement speed
     public float ballSpeed = 100.0f;
 
-    //this runs as soon as the scene starts
+    
     void Start()
     {
-        //this sets the rigidbody of the ball to the ball speed on start, then once set it stays set.
+        // Get the ball's rigid body
         GetComponent<Rigidbody2D>().velocity = Vector2.up * ballSpeed;
+
+		GetComponent<Rigidbody2D>().AddTorque(0.5f);
     }
 
-    //this is a custom function to determine how far along the ball hit the paddle
-    //it works by taking the ball position, the paddle pos and the width of the paddle.
+    
     float ballAngle(Vector2 ballPos, Vector2 paddlePos, float paddleWidth)
     {
-        //by minusing the position of the ball from the paddle you are left with a direction
-        //then by diving that by the width of the paddle, you are left with the angle
+       
         return (ballPos.x - paddlePos.x) / paddleWidth;
     }
 
-    //this runs whenever the ball collides with anthing
+    
    void OnCollisionEnter2D(Collision2D collision)
     {
-        //if the ball collides with a gameobject with the name Paddle the code inside will run
+        
         if (collision.gameObject.name == "Paddle")
         {
-            //this passes the details of the collision to that function we made earlier.
+            // Calculate the angle.
             float x = ballAngle(transform.position, collision.transform.position, collision.collider.bounds.size.x);
 
-            //this normalises the direction
+            // Normalize that shit.
             Vector2 dir = new Vector2(x, 1).normalized;
 
-            //this gets the velocity of the ball and changes it based on how it hit the paddle.
+            // Calculate Velocity.
             GetComponent<Rigidbody2D>().velocity = dir * ballSpeed;
         }
     }
--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -4,40 +4,120 @@ using System.Collections;
 public class Paddle : MonoBehaviour {
 
     //this is the movement speed of the paddle
-    public float movementSpeed = 150;
+
+    public float runningSpeed = 150;
+	public float sprintingSpeed = 300;
+	private float speed = 150;
 	public Animator guyAnim;
 
     //this creates a reference to the rigidbody on the paddle
     public Rigidbody2D rb2d;
 
+	private bool facingLeft  = false;
+	private bool isRunning	 = false;
+	private bool isSliding 	 = false;
+
+	// sliding vars
+	public float slideTimerMax = 0.15f;
+	private float slideTimer = 0f;
+	private Vector3 slideForward;
+
+	void Start(){
+		rb2d = this.GetComponent<Rigidbody2D>();
+	}
+
+
     //this runs every frame. even if their computer is slower or fasted this will run the same speed.
-    void FixedUpdate()
+    void Update()
     {
+
+		if (Input.GetKey(KeyCode.LeftShift)){
+			speed = sprintingSpeed;
+		}
+		else{
+			speed = runningSpeed;
+		}
+
+		// jump
+//		if (Input.GetKeyDown(KeyCode.Space)){
+//			Debug.Log("jump");
+//			rb2d.AddForce(Vector2.up * 20000);
+//		}
+
+
 
         //get horizontal movement from axis
         float hor = Input.GetAxisRaw("Horizontal");
 
         // Set Velocity (movement direction * speed)
-        rb2d.velocity = Vector2.right * hor * movementSpeed;
+        rb2d.velocity = Vector2.right * hor * speed;
+
+
     }
 
-	void Update(){
+	void FixedUpdate(){
 
-		if(rb2d.velocity.x > 0f){
-			guyAnim.SetBool("running",true);
-			//guyAnim.transform.rotation = new Vector3(0f,0,0f);
-		}
-		else if (rb2d.velocity.x < 0f){
-			guyAnim.SetBool("running",true);
+	
 
-			if (guyAnim.transform.rotation.y != 180.0f){
+		// Sprite direction
+		if(Input.GetKey(KeyCode.D)){
+			guyAnim.SetBool("running",true);
+			isRunning = true;
+			slideForward = transform.right;
+
+			if (facingLeft){
 				guyAnim.transform.Rotate(new Vector3(0f,180f,0f));
+				facingLeft = false;
+
+
+			}
+		}
+		else if (Input.GetKey(KeyCode.A)){
+			guyAnim.SetBool("running",true);
+			isRunning = true;
+			slideForward = transform.right * -1;
+
+			if (!facingLeft){
+				guyAnim.transform.Rotate(new Vector3(0f,180f,0f));
+				facingLeft = true;
+
 			}
 
 		}
-		else if (rb2d.velocity.x == 0f){
+
+		else if (isRunning){
+
+		
+			// ANIMATION: running => slide => idle
 			guyAnim.SetBool("running",false);
+
+			// push the guy a little further to "slide"
+			if (speed == sprintingSpeed)
+				isSliding = true;
+
+			isRunning = false;
+
 		}
+
+
+		if (isSliding){
+			Debug.Log("sliding");
+
+			rb2d.velocity = slideForward * runningSpeed;
+
+			slideTimer += Time.deltaTime;
+			if(slideTimer > slideTimerMax){
+				isSliding = false;
+				slideTimer = 0;
+				//isRunning = false;
+			}
+
+
+		}
+
+
+
+
 
 	}
 }
